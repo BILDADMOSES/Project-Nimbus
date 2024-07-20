358afb46c5c09662c02133245be0b2c24479bc03
BILDADMOSES <bildadmoses8@gmail.com>
2024-07-20 20:57:15 +0300

    Updated language selection

--- components/LanguageSelect.tsx
+++ components/LanguageSelect.tsx
@@ -6,13 +6,29 @@ interface LanguageSelectProps {
 }
 
 const languages: Language[] = [
-  { code: 'en', name: 'English' },
-  { code: 'es', name: 'Spanish' },
-  { code: 'fr', name: 'French' },
-  { code: 'de', name: 'German' },
-  { code: 'zh', name: 'Chinese' },
-  //more 
-];
+  const languages: Language[] = [
+    { code: 'en', name: 'English' },
+    { code: 'es', name: 'Spanish' },
+    { code: 'fr', name: 'French' },
+    { code: 'de', name: 'German' },
+    { code: 'zh', name: 'Chinese' },
+    { code: 'sw', name: 'Swahili' },
+    { code: 'ar', name: 'Arabic' },
+    { code: 'ha', name: 'Hausa' },
+    { code: 'om', name: 'Oromo' },
+    { code: 'ig', name: 'Igbo' },
+    { code: 'am', name: 'Amharic' },
+    { code: 'pt', name: 'Portuguese' },
+    { code: 'so', name: 'Somali' },
+    { code: 'sn', name: 'Shona' },
+    { code: 'ber', name: 'Berber' },
+    { code: 'yo', name: 'Yoruba' },
+    { code: 'ff', name: 'Fulani' },
+    { code: 'zu', name: 'Zulu' },
+    { code: 'pcm', name: 'Pidgin English' }
+    //more
+  ];
+  
 
 export const LanguageSelect: React.FC<LanguageSelectProps> = ({ selectedLanguage, onSelect }) => {
   return (
