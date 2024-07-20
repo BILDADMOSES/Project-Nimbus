1ec0bd0f17cd778565cbdd03469f7c9e103b795b
BILDADMOSES <bildadmoses8@gmail.com>
2024-07-20 20:58:00 +0300

    Updated language selection

--- components/LanguageSelect.tsx
+++ components/LanguageSelect.tsx
@@ -6,29 +6,28 @@ interface LanguageSelectProps {
 }
 
 const languages: Language[] = [
-  const languages: Language[] = [
-    { code: 'en', name: 'English' },
-    { code: 'es', name: 'Spanish' },
-    { code: 'fr', name: 'French' },
-    { code: 'de', name: 'German' },
-    { code: 'zh', name: 'Chinese' },
-    { code: 'sw', name: 'Swahili' },
-    { code: 'ar', name: 'Arabic' },
-    { code: 'ha', name: 'Hausa' },
-    { code: 'om', name: 'Oromo' },
-    { code: 'ig', name: 'Igbo' },
-    { code: 'am', name: 'Amharic' },
-    { code: 'pt', name: 'Portuguese' },
-    { code: 'so', name: 'Somali' },
-    { code: 'sn', name: 'Shona' },
-    { code: 'ber', name: 'Berber' },
-    { code: 'yo', name: 'Yoruba' },
-    { code: 'ff', name: 'Fulani' },
-    { code: 'zu', name: 'Zulu' },
-    { code: 'pcm', name: 'Pidgin English' }
-    //more
-  ];
-  
+  { code: 'en', name: 'English' },
+  { code: 'es', name: 'Spanish' },
+  { code: 'fr', name: 'French' },
+  { code: 'de', name: 'German' },
+  { code: 'zh', name: 'Chinese' },
+  { code: 'sw', name: 'Swahili' },
+  { code: 'ar', name: 'Arabic' },
+  { code: 'ha', name: 'Hausa' },
+  { code: 'om', name: 'Oromo' },
+  { code: 'ig', name: 'Igbo' },
+  { code: 'am', name: 'Amharic' },
+  { code: 'pt', name: 'Portuguese' },
+  { code: 'so', name: 'Somali' },
+  { code: 'sn', name: 'Shona' },
+  { code: 'ber', name: 'Berber' },
+  { code: 'yo', name: 'Yoruba' },
+  { code: 'ff', name: 'Fulani' },
+  { code: 'zu', name: 'Zulu' },
+  { code: 'pcm', name: 'Pidgin English' }
+  //more
+];
+
 
 export const LanguageSelect: React.FC<LanguageSelectProps> = ({ selectedLanguage, onSelect }) => {
   return (
